0038f84e9aa8333e10ba610f8c6a249d56cd2f5a
Salvador Pardiñas <darkfm@vera.com.uy>
2017-04-09 03:38:54 -0300

    Created page

---
+++ docs/Marisa.js
@@ -0,0 +1,11 @@
+document.write('\
+\
+<h1 id="mariscobot">MariscoBot</h1>\
+<p>MariscoBot is a GPLv3 discord.js bot to communicate with Lord Marisa.</p>\
+<h2 id="links">Links</h2>\
+<p><a href="https://github.com/ForrosInc/MariscoBot">github</a></p>\
+<p><a href="docs/">Documentation</a></p>\
+<h2 id="license">License</h2>\
+<p>MariscoBot is licensed under <a href="https://www.gnu.org/licenses/gpl-3.0-standalone.html">GPLv3</a>.</p>\
+\
+');
---
+++ docs/Marisa.md
@@ -0,0 +1,10 @@
+#MariscoBot
+MariscoBot is a GPLv3 discord.js bot to communicate with Lord Marisa.
+
+##Links
+[github](https://github.com/ForrosInc/MariscoBot)
+
+[Documentation](docs/)
+
+##License
+MariscoBot is licensed under [GPLv3](https://www.gnu.org/licenses/gpl-3.0-standalone.html).
