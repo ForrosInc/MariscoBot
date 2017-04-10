36d2bda0a18202fd6bf654f3cd19ca93159628a1
Salvador Pardiñas <darkfm@vera.com.uy>
2017-04-10 01:57:01 -0300

    Blank line fix Blank line made ePub and HTML documentation fuck up

---
+++ docs/docs/images.pl
@@ -0,0 +1,163 @@
+# LaTeX2HTML 2017.2 (Released Jan 23, 2017)
+# Associate images original text with physical files.
+
+
+$key = q/framebox[1.1width]{"guildidhere"};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="103" HEIGHT="25" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img20.png"
+ ALT="\framebox[1.1\width]{''guildidhere''}">|; 
+
+$key = q/framebox[1.1width]{"slashdumpguildid"};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="124" HEIGHT="27" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img21.png"
+ ALT="\framebox[1.1\width]{''/dumpguildid''}">|; 
+
+$key = q/framebox[1.1width]{#Dango};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="65" HEIGHT="25" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img7.png"
+ ALT="\framebox[1.1\width]{\char93 Dango}">|; 
+
+$key = q/framebox[1.1width]{#Hecatia};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="73" HEIGHT="25" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img5.png"
+ ALT="\framebox[1.1\width]{\char93 Hecatia}">|; 
+
+$key = q/framebox[1.1width]{#Marisa};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="68" HEIGHT="25" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img4.png"
+ ALT="\framebox[1.1\width]{\char93 Marisa}">|; 
+
+$key = q/framebox[1.1width]{#Mokou};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="68" HEIGHT="25" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img10.png"
+ ALT="\framebox[1.1\width]{\char93 Mokou}">|; 
+
+$key = q/framebox[1.1width]{#OHIO};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="62" HEIGHT="25" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img6.png"
+ ALT="\framebox[1.1\width]{\char93 OHIO}">|; 
+
+$key = q/framebox[1.1width]{#OUTBEAST};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="115" HEIGHT="25" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img3.png"
+ ALT="\framebox[1.1\width]{\char93 OUTBEAST}">|; 
+
+$key = q/framebox[1.1width]{#SpreadMarisco};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="134" HEIGHT="25" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img8.png"
+ ALT="\framebox[1.1\width]{\char93 Spread Marisco}">|; 
+
+$key = q/framebox[1.1width]{#Talk};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="50" HEIGHT="25" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img2.png"
+ ALT="\framebox[1.1\width]{\char93 Talk}">|; 
+
+$key = q/framebox[1.1width]{#Waifu};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="61" HEIGHT="25" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img9.png"
+ ALT="\framebox[1.1\width]{\char93 Waifu}">|; 
+
+$key = q/framebox[1.1width]{#help};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="48" HEIGHT="25" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img1.png"
+ ALT="\framebox[1.1\width]{\char93 help}">|; 
+
+$key = q/framebox[1.1width]{'DiscordkeyHere'};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="142" HEIGHT="25" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img24.png"
+ ALT="\framebox[1.1\width]{'Discord key Here'}">|; 
+
+$key = q/framebox[1.1width]{Danbooru:dollarx};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="100" HEIGHT="24" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img13.png"
+ ALT="\framebox[1.1\width]{Danbooru:\$x}">|; 
+
+$key = q/framebox[1.1width]{LANG};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="52" HEIGHT="22" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img22.png"
+ ALT="\framebox[1.1\width]{LANG}">|; 
+
+$key = q/framebox[1.1width]{M-playdollarx};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="79" HEIGHT="26" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img12.png"
+ ALT="\framebox[1.1\width]{M-play \$x}">|; 
+
+$key = q/framebox[1.1width]{SummonMarisco};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="134" HEIGHT="22" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img11.png"
+ ALT="\framebox[1.1\width]{Summon Marisco}">|; 
+
+$key = q/framebox[1.1width]{key:dollarx};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="57" HEIGHT="26" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img16.png"
+ ALT="\framebox[1.1\width]{key: \$x}">|; 
+
+$key = q/framebox[1.1width]{maxResults:dollarx};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="120" HEIGHT="24" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img15.png"
+ ALT="\framebox[1.1\width]{maxResults: \$x}">|; 
+
+$key = q/framebox[1.1width]{varLANG=dollarx};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="125" HEIGHT="24" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img18.png"
+ ALT="\framebox[1.1\width]{var LANG = \$x}">|; 
+
+$key = q/{lstlisting}[frame=single,inputencoding=utf8,showstringspaces=false,linewidth=18==="slashdumpguildid")console.log("GuildIDis"+msg.guild.id+"n");{lstlisting};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="840" HEIGHT="50" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img19.png"
+ ALT="\begin{lstlisting}[frame=single,inputencoding=utf8,showstringspaces=false,linewi...
+...ildid'') console.log(''Guild ID is '' + msg.guild.id + ''\n'');
+\end{lstlisting}">|; 
+
+$key = q/{lstlisting}[frame=single,inputencoding=utf8,showstringspaces=false]client.login('DiscordkeyHere');{lstlisting};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="567" HEIGHT="31" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img23.png"
+ ALT="\begin{lstlisting}[frame=single,inputencoding=utf8,showstringspaces=false]
+client.login('Discord key Here');
+\end{lstlisting}">|; 
+
+$key = q/{lstlisting}[frame=single,inputencoding=utf8,showstringspaces=false]constSPANISH=0;constENGLISH=1;varLANG=SPANISH;{lstlisting};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="567" HEIGHT="69" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img17.png"
+ ALT="\begin{lstlisting}[frame=single,inputencoding=utf8,showstringspaces=false]
+const SPANISH = 0;
+const ENGLISH = 1;
+var LANG = SPANISH;
+\end{lstlisting}">|; 
+
+$key = q/{lstlisting}[frame=single,inputencoding=utf8,showstringspaces=false]varoptsyt={maxResults:1,key:'YTKeyHere'};{lstlisting};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="567" HEIGHT="88" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img14.png"
+ ALT="\begin{lstlisting}[frame=single,inputencoding=utf8,showstringspaces=false]
+var optsyt = {
+maxResults: 1,
+key: 'YTKeyHere'
+};
+\end{lstlisting}">|; 
+
+1;
+
